a60d12c29bf3d9b19425dd3df5a337d9cab11f8b
QuantuMope <neffneff4@gmail.com>
2020-03-10 04:06:40 -0700

    get imputation to 89%

--- phaser.py
+++ phaser.py
@@ -1,6 +1,5 @@
 import numpy as np
 import sys
-import csv
 import random
 
 
@@ -8,27 +7,17 @@ def main():
     data = np.loadtxt(fname=sys.argv[1])
     num_individuals, num_snps = data.shape
 
-    # Transition probabilities of next SNP
-    # Probability that the next SNP is either a 0 or 2 when prev SNP was 0, 1, 2
-    trans_probs_1 = np.zeros((num_snps, 6))
-
-    # trans probs of next SNP when prev two SNPs were either 00, 01, 02, 10, 11, 12, 20, 21, 22
-    trans_probs_2 = np.zeros((num_snps, 18))
-
-    # for i in range(num_snps):
-    #     _, counts = np.unique(data[:, i], return_counts=True)
-    #     allele_counts[i, :] = counts
-
     for i in range(num_snps):
         if i == 0:
             masked = np.where(data[:, 0] == 9)[0]
             for mask in masked:
                 data[mask, 0] = random.choice([0, 2])
+
+        elif i == 1:
+            trans_probs_1 = np.zeros((num_snps, 6))
             prev_zeroes = np.where(data[:, 0] == 0)[0]
             prev_ones = np.where(data[:, 0] == 1)[0]
             prev_twos = np.where(data[:, 0] == 2)[0]
-
-        elif i == 1:
             next_from_zeros = data[prev_zeroes, i]
             next_from_ones = data[prev_ones, i]
             next_from_twos = data[prev_twos, i]
@@ -84,16 +73,19 @@ def main():
                 counts.append(len(np.where(snp == 0)[0]))
                 counts.append(len(np.where(snp == 2)[0]))
 
+            trp = []
             for j in range(0, len(counts), 2):
                 total = counts[j] + counts[j+1]
-                trans_probs_2[i, j] = counts[j] / total
-                trans_probs_2[i, j+1] = counts[j+1] / total
-
-            trp = trans_probs_2
+                if total == 0:
+                    trp.append(0)
+                    trp.append(0)
+                    continue
+                trp.append(counts[j] / total)
+                trp.append(counts[j+1] / total)
 
             assigns = []
-            for j in range(0, len(trp[i]), 2):
-                assigns.append(0 if trp[i, j] > trp[i, j+1] else 2)
+            for j in range(0, len(trp), 2):
+                assigns.append(0 if trp[j] > trp[j+1] else 2)
 
             masked = np.where(data[:, i] == 9)[0]
 
@@ -105,7 +97,7 @@ def main():
                         if pp == z and p == j:
                             data[mask, i] = assigns[counter]
                         counter += 1
-        else:
+        elif i == 3:
             # 27 total possible permutations
             prev_indexes = []
             for p in range(3):
@@ -127,13 +119,16 @@ def main():
             trp = []
             for j in range(0, len(counts), 2):
                 total = counts[j] + counts[j + 1]
+                if total == 0:
+                    trp.append(0)
+                    trp.append(0)
+                    continue
                 trp.append(counts[j] / total)
                 trp.append(counts[j+1] / total)
 
             assigns = []
             for j in range(0, len(trp), 2):
                 assigns.append(0 if trp[j] > trp[j+1] else 2)
-                assert abs((trp[j] + trp[j+1]) - 1.) < 0.01
 
             masked = np.where(data[:, i] == 9)[0]
 
@@ -146,6 +141,104 @@ def main():
                             if ppp == z and pp == j and p == t:
                                 data[mask, i] = assigns[counter]
                             counter += 1
+
+        elif i == 4:
+            # 64 total possible permutations
+            prev_indexes = []
+            for p in range(3):
+                for j in range(3):
+                    for t in range(3):
+                        for z in range(3):
+                            prev_indexes.append(np.where(np.logical_and.reduce((data[:, i-4] == p,
+                                                                                data[:, i-3] == j,
+                                                                                data[:, i-2] == t,
+                                                                                data[:, i-1] == z)))[0])
+
+            snps = []
+            for prev in prev_indexes:
+                snps.append(data[prev, i])
+
+            counts = []
+            for snp in snps:
+                counts.append(len(np.where(snp == 0)[0]))
+                counts.append(len(np.where(snp == 2)[0]))
+
+            trp = []
+            for j in range(0, len(counts), 2):
+                total = counts[j] + counts[j + 1]
+                if total == 0:
+                    trp.append(0)
+                    trp.append(0)
+                    continue
+                trp.append(counts[j] / total)
+                trp.append(counts[j+1] / total)
+
+            assigns = []
+            for j in range(0, len(trp), 2):
+                assigns.append(0 if trp[j] > trp[j+1] else 2)
+
+            masked = np.where(data[:, i] == 9)[0]
+
+            for mask in masked:
+                counter = 0
+                pppp, ppp, pp, p = data[mask, i-4:i]
+                for z in range(3):
+                    for j in range(3):
+                        for t in range(3):
+                            for n in range(3):
+                                if pppp == z and ppp == j and pp == t and p == n:
+                                    data[mask, i] = assigns[counter]
+                                counter += 1
+        else:
+            # 125 total possible permutations
+            prev_indexes = []
+            for p in range(3):
+                for j in range(3):
+                    for t in range(3):
+                        for z in range(3):
+                            for y in range(3):
+                                prev_indexes.append(np.where(np.logical_and.reduce((data[:, i-5] == p,
+                                                                                    data[:, i-4] == j,
+                                                                                    data[:, i-3] == t,
+                                                                                    data[:, i-2] == z,
+                                                                                    data[:, i-1] == y)))[0])
+
+            snps = []
+            for prev in prev_indexes:
+                snps.append(data[prev, i])
+
+            counts = []
+            for snp in snps:
+                counts.append(len(np.where(snp == 0)[0]))
+                counts.append(len(np.where(snp == 2)[0]))
+
+            trp = []
+            for j in range(0, len(counts), 2):
+                total = counts[j] + counts[j + 1]
+                if total == 0:
+                    trp.append(0)
+                    trp.append(0)
+                    continue
+                trp.append(counts[j] / total)
+                trp.append(counts[j+1] / total)
+
+            assigns = []
+            for j in range(0, len(trp), 2):
+                assigns.append(0 if trp[j] > trp[j+1] else 2)
+
+            masked = np.where(data[:, i] == 9)[0]
+
+            for mask in masked:
+                counter = 0
+                ppppp, pppp, ppp, pp, p = data[mask, i-5:i]
+                for z in range(3):
+                    for j in range(3):
+                        for t in range(3):
+                            for n in range(3):
+                                for y in range(3):
+                                    if ppppp == z and pppp == j and ppp == t and pp == n and p == y:
+                                        data[mask, i] = assigns[counter]
+                                    counter += 1
 
         print("Processing SNP {}".format(i), flush=True)
 
