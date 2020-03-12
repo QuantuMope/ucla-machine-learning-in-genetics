79c75815ac6af3a81b1a7bbeceb67d656aaddb24
QuantuMope <neffneff4@gmail.com>
2020-03-12 00:04:12 -0700

    get 66% switch accuracy

--- phaser.py
+++ phaser.py
@@ -65,19 +65,20 @@ def get_unique_loop(data, num_individuals):
 
 
 def clarks_algo(data, num_individuals, num_snps, num_blocks, block_size):
-    phased_haplotypes = np.zeros((num_individuals, num_snps*2))
+    phased_haplotypes = np.zeros((num_individuals*2, num_snps))
     phased_haplotypes[:] = 9  # no entry should be a 9 at the end
     unique_haplos = []
     remainder = num_snps % block_size
+    full_match, half_match, no_match = 0, 0, 0
     for b in range(num_blocks):
         unique_haplos.clear()
         h1s = b*block_size
         h1e = (b+1)*block_size
-        h2s = b*block_size + num_snps
-        h2e = (b+1)*block_size + num_snps
+        # h2s = b*block_size + num_snps
+        # h2e = (b+1)*block_size + num_snps
         if remainder != 0 and b == num_blocks-1:
             h1e = h1s + remainder
-            h2e = h2s + remainder
+            # h2e = h2s + remainder
             block_size = remainder
         for i in range(num_individuals):
             geno_snp = data[i, h1s:h1e]
@@ -85,35 +86,43 @@ def clarks_algo(data, num_individuals, num_snps, num_blocks, block_size):
             found_match = False
             if i == 0:
                 haplo_1, haplo_2 = phase_from_geno(geno_snp, unique_haplos, block_size)
-                phased_haplotypes[i, h1s:h1e] = haplo_1
-                phased_haplotypes[i, h2s:h2e] = haplo_2
+                phased_haplotypes[2*i, h1s:h1e] = haplo_1
+                phased_haplotypes[2*i+1, h1s:h1e] = haplo_2
                 continue
             # Try to find a match with existing haplotypes
             for x, haplo_1 in enumerate(unique_haplos):
                 for y, haplo_2 in enumerate(unique_haplos, start=x):
                     if np.array_equal(geno_snp, haplo_1 + haplo_2):
-                        phased_haplotypes[i, h1s:h1e] = haplo_1
-                        phased_haplotypes[i, h2s:h2e] = haplo_2
+                        full_match += 1
+                        phased_haplotypes[2*i, h1s:h1e] = haplo_1
+                        phased_haplotypes[2*i+1, h1s:h1e] = haplo_2
                         found_match = True
                         break
                 if found_match:
                     break
-            # If no matches with current list.
+            if found_match:
+                continue
+            # If no matches with current list, find new complement.
             for haplo in unique_haplos:
                 potential_haplo = geno_snp - haplo
                 if not np.any(potential_haplo == 2) and not np.any(potential_haplo == -1):
-                    print('half match')
+                    half_match += 1
                     unique_haplos.append(potential_haplo)
-                    phased_haplotypes[i, h1s:h1e] = haplo
-                    phased_haplotypes[i, h2s:h2e] = potential_haplo
+                    phased_haplotypes[2*i, h1s:h1e] = haplo
+                    phased_haplotypes[2*i+1, h1s:h1e] = potential_haplo
                     found_match = True
                     break
+            # If no possible initial pair, add two new haplos.
             if not found_match:
+                no_match += 1
                 haplo_1, haplo_2 = phase_from_geno(geno_snp, unique_haplos, block_size)
-                phased_haplotypes[i, h1s:h1e] = haplo_1
-                phased_haplotypes[i, h2s:h2e] = haplo_2
+                phased_haplotypes[2*i, h1s:h1e] = haplo_1
+                phased_haplotypes[2*i+1, h1s:h1e] = haplo_2
 
-        print(len(unique_haplos))
+        # print(len(unique_haplos))
+        # print('full_match: {}'.format(full_match))
+        # print('half_match: {}'.format(half_match))
+        # print('no_match: {}'.format(no_match))
 
     assert not np.any(phased_haplotypes == 9)
     assert not np.any(phased_haplotypes == 2)
@@ -140,22 +149,21 @@ def phase_from_geno(geno_snp, unique_haplos, block_size):
     return haplo_1, haplo_2
 
 
-
 def main():
     data = np.loadtxt(fname=sys.argv[1])
     data = data.T
     num_individuals, num_snps = data.shape
 
-    size = 238
+    size = num_snps
+    block_size = 4
+
     sample_genotype = data[:, :size]
-    block_size = 5
     num_blocks = ceil(size / block_size)
-    print(num_blocks)
     num_snps = size
     phased_haplotypes = clarks_algo(sample_genotype, num_individuals, num_snps, num_blocks, block_size)
-    print(phased_haplotypes)
+    print(phased_haplotypes.T.shape)
 
-    np.savetxt('phased', data.T, fmt='%d', delimiter=' ')
+    np.savetxt('phased', phased_haplotypes.T, fmt='%d', delimiter=' ')
 
 
 if __name__ == '__main__':
